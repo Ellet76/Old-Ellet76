431f22e41047d5c4aeba7f0fcc35b793927537c0
Linus Scott <linsco@edu.umea.se>
2023-10-18 09:30:08 +0200

    Start: Done

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,39 @@
-
-<h1>
-  Välkommen till min hemsida!
+<h1 class="name">
+	Välkommen till min hemsida!
 </h1>
 
-<h2>Här har jag samlat min uppgifter jag gjort i samband med webbutveckling i årskurs 2 på Maja Beskowgymnasiet!</h2>
+<h2 class="name">
+	Här har jag samlat mina uppgifter jag gjort i samband med webbutveckling i årskurs 2 på Maja Beskowgymnasiet!
+</h2>
 
-<h3 >Förnamn Efternamn</h3>
+<h3 class="name">
+	Linus Scott
+</h3>
+
+<p id="inspo">
+	"The only way to learn a new programming language is by writing
+	programs in it."
+	<br> - Dennis Ritchie, creator of the C programming language.
+</p>
+
+<p style="background-color:pink">
+	"Every great developer you know got there by solving problems they
+	were unqualified to solve until they actually did it." - Patrick McKenzie,
+	software engineer and entrepreneur.
+</p>
+
+
+<style>
+.name{
+	color:black;
+	font-family:'Gill Sans';
+}
+#inspo{
+	font-family: 'Courier';
+	font-size: 160%;
+}
+h1, h2, h3, p{
+	display: block;
+}
+</style>
 
