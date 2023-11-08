1e5c7fe739596339a8c87f083b95d74570557238
Linus Scott <linsco@edu.umea.se>
2023-11-08 10:50:10 +0100

    register fix

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,35 +1,23 @@
 <script>
-    let firstName;
-    let lastName;
     let email;
     let password;
-    let food;
     
 function handleSubmit()
 {
     //Handling form submit 
-    console.log(firstName, lastName, email, password, food)
+    console.log(email, password)
     console.log("heheheha")
 }
 </script>
 
-<h1>Registration form</h1>
+<h1>Login form</h1>
 <div>
     <form on:submit|preventDefault={handleSubmit}>
-        <label for="firstName">First Name: </label>
-        <input id="firstName" type="text" bind:value={firstName}>
-
-        <label for="lastName">Last Name: </label>
-        <input id="lastName" type="text" bind:value={lastName}>
-
         <label for="email">Email: </label>
         <input id="email" type="text" bind:value={email}>
 
         <label for="password">Password: </label>
         <input id="password" type="text" bind:value={password}>
-
-        <label for="food">Favourite Food: </label>
-        <input  id="food" type="text" bind:value={food}>
         
         <button type="submit">Log in</button>
     </form>
