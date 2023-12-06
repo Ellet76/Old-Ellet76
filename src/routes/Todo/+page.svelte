331b250a85048336cdb8076822fc95de501dfc5f
Linus Scott <linsco@edu.umea.se>
2023-12-06 12:32:07 +0100

    added Todo

--- src/routes/Todo/+page.svelte
+++ src/routes/Todo/+page.svelte
@@ -1,38 +1,108 @@
 <script>
-    import { Input } from "postcss";
+    import { onMount } from "svelte";
+    let not_Done = [];
+    let done = []
+    
 
+    function Add()
+    {
+        const add = document.getElementById("task").value;
+        not_Done = [...not_Done, add]
+    }
 
+    function RemoveFromNotDone(index)
+    {
+        not_Done = not_Done.filter((_, i) => i !== index);
+    }
+    function SetItemToDone(index)
+    {
+        done = [...done, not_Done[index]]
+        not_Done = not_Done.filter((_, i) => i !== index);
+    }
+    function RemoveFromDone(index)
+    {
+        done = done.filter((_, i) => i !== index);
+    }
+    function SetItemToNotDone(index)
+    {
+        not_Done = [...not_Done, done[index]]
+        done = done.filter((_, i) => i !== index);
+    }
+    onMount(() => {})
 </script>
 
 <div class="container">
     <h1 class="text">
-        Todo
+        Todo list
     </h1>
     <div class="layout">
         <div style="display: grid">
             <h1 class="text">New</h1>
-            <ol>
-                <li>objekt 1</li>
-                <li>objekt 2</li>
-            </ol>
-            <input type="text">
+            
+            <div class="list">
+                <ol>
+                    {#each not_Done as item (item)}
+                    <li>{item}</li>
+                    {/each}
+                </ol>
+                <ul>
+                    {#each not_Done as item, index (item)}
+                        <li>
+                            <button id="list_button" on:click={() => SetItemToDone(index)}>Done</button>
+                        </li>
+                    {/each}
+                </ul>
+                <ul>
+                    {#each not_Done as item, index (item)}
+                        <li>
+                            <button id="list_button" on:click={() => RemoveFromNotDone(index)}>Remove</button>
+                        </li>
+                    {/each}
+                </ul>
+            </div>
+            <div class="submit">
+                <input id="task" type="text">
+                <button on:click={Add}>Submit</button>
+            </div>
         </div>
+
         <div style="display: grid;">
             <h1 class="text">Done</h1>
-            <input type="text">
+            <div class="list">
+                <ol>
+                    {#each done as item (item)}
+                    <li>{item}</li>
+                    {/each}
+                </ol>
+                <ul>
+                    {#each done as item, index (item)}
+                        <li>
+                            <button id="list_button" on:click={() => SetItemToNotDone(index)}>Done</button>
+                        </li>
+                    {/each}
+                </ul>
+                <ul>
+                    {#each done as item, index (item)}
+                        <li>
+                            <button id="list_button" on:click={() => RemoveFromDone(index)}>Remove</button>
+                        </li>
+                    {/each}
+                </ul>
+            </div>
         </div>
     </div>
 </div>
 
 <style>
     .container{
-        width: 80vw;
-        height: 70vh;
+        width: 98%;
+        height: 85vh;
         background-color: beige;
         border-width: 10px;
         border-style: solid;
         border-color: black;
         padding-top: 10px;
+        margin: auto;
     }
 
     .layout{
@@ -40,6 +110,21 @@
         width: 100%;
         grid-template-columns: 50% 50%;
     }
+    .list{
+        display: grid;
+        width: 100%;
+        grid-template-columns: 4fr 1fr 1fr;
+    }
+    #list_button{
+        width: 100%;
+        text-align: center;
+    }
+    .submit{
+        display: grid;
+        width: 100%;
+        grid-template-columns: 90% 10%;
+    }
+
     .text{
         padding-left: 20px;
     }
@@ -48,9 +133,13 @@
         list-style-type: upper-roman;
     }
 
+    ul{
+        list-style: none;
+    }
+
     input:focus{
         border-color: black;
-        background-color: beige;
+        background-color: rgb(185, 255, 245)
     }
     input{
         transition: border-color 0.5s, background-color 0.5s;
