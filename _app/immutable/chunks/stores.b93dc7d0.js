6eb35611678b1767c2196f014078776d1c7195df
Ellet76 <122363941+Ellet76@users.noreply.github.com>
2023-12-15 09:58:33 +0000

    Deploying to gh-pages from @ Ellet76/Ellet76.github.io@e60e026424a13c212b6003a0876f3a6cf048b979 🚀

---
+++ _app/immutable/chunks/stores.b93dc7d0.js
@@ -0,0 +1,1 @@
+import"./index.d224f26d.js";import{d as e}from"./singletons.aacba446.js";const r=()=>{const s=e;return{page:{subscribe:s.page.subscribe},navigating:{subscribe:s.navigating.subscribe},updated:s.updated}},b={subscribe(s){return r().page.subscribe(s)}};export{b as p};
