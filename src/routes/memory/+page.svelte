e60e026424a13c212b6003a0876f3a6cf048b979
Linus Scott <linsco@edu.umea.se>
2023-12-15 10:58:05 +0100

    added working memory

--- src/routes/memory/+page.svelte
+++ src/routes/memory/+page.svelte
@@ -1,36 +1,92 @@
 <script>
-  let cards = [];
-  for (let index = 0; index < 12; index++) {
-    cards.push({
-      id: 5, // TODO: unique ids per card card
-      img: "/notrick.gif", // TODO: unique images per card card
-      flipped: false,  // TODO: think
-      completed: false,
-    });
-  }
-  let flipcount = 0;
   function flip(card) {
-    // flip card over if two cards are not already flipped
-    // TODO: and card is already not flipped
-    if (card.flipped && flipcount < 2) {
+    if (!card.flipped && flipcount < 2) {
       // TODO: Probably do what?
-
+      card.flipped = true;
+      flipcount += 1;
+      flipped_cards.push(card);
       // flip the cards over after 2s from seeing both cards
-      if (flipcount == 4) {
+      if (flipcount == 2) {
         setTimeout(() => {
           // flip over cards that have not been marked as "completed"
+          if (flipped_cards[0].id == flipped_cards[1].id){
+            flipped_cards[0].completed = true
+            flipped_cards[1].completed = true
+            if (active_player == "blue"){
+              blue_player += 1
+            }
+            else if (active_player == "red"){
+              red_player += 1
+            }
+            console.log(active_player)
+          }
+          cards = cards;
+          flipped_cards = []
           cards.forEach((card) => {
             card.flipped = card.completed;
           });
           flipcount = 0;
-          cards = cards;
-        }, 2000);
+          if (active_player == "blue") {
+            active_player = "red"
+            console.log(active_player, "1")
+          } else if (active_player == "red") {
+            active_player = "blue"
+            console.log(active_player, "2")
+          }
+          
+        }, 1000);
       }
+      console.log(cards)
       cards = cards;
+
     } else {
       alert("chill");
     }
   }
+
+  function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+  }
+
+  let cards = [];
+  let flipped_cards = []
+  let flipcount = 0  
+  let red_player = 0;
+  let blue_player = 0;
+  let active_player = "blue"
+  let images = ["Image_1.jpg", "Image_2.jpg", "Image_3.jpg", "Image_4.jpg", "Image_5.jpg", "Image_6.jpg"]
+
+
+  function StartUp(){
+    cards = [];
+    flipped_cards = []
+    flipcount = 0  
+    red_player = 0;
+    blue_player = 0;
+    active_player = "blue"
+
+    for (let index = 0; index < 12/2; index++) {
+      cards.push({
+        id: index,
+        img: images[index], // TODO: unique images per card card
+        flipped: false,  // TODO: think
+        completed: false,
+      });
+
+      cards.push({
+        id: index,
+        img: images[index], // TODO: unique images per card card
+        flipped: false,  // TODO: think
+        completed: false,
+      });
+    }
+
+    shuffleArray(cards)
+  }
+  StartUp()
 </script>
 
 <main>
@@ -51,7 +107,17 @@
       </div>
     {/each}
   </div>
+
+  <div id="red-box" class="box"> <p>{red_player}</p> </div>
+  <div id="blue-box" class="box"> <p>{blue_player}</p> </div>
+  <div id="turn-box" class="box" style={active_player=="blue" ? "right: 10px;":"left:10px"}> </div>
+
+  <div class="box" style="bottom: 0px;">
+    <button type="button" style="width: 50px; height: 50px;" on:click={StartUp}> Reset</button>
+  </div>
+
 </main>
+
 
 <style>
   main {
@@ -113,5 +179,37 @@
     backface-visibility: hidden;
     -webkit-backface-visibility: hidden;
     position: absolute;
+  }
+
+  .box{
+    width: 100px;
+    height: 100px;
+    position: fixed;
+    text-align: center;
+    font-size: 30px;
+  }
+
+  #red-box, #blue-box{
+    bottom: 0px;
+    z-index: 2;
+    width: 100px;
+    height: 100px;
+  }
+
+  #red-box{
+    background-color: red;
+    left: 0px;
+  }
+
+  #blue-box{
+    background-color: blue;
+    right: 0px;
+  }
+
+  #turn-box {
+    bottom: 10px;
+    z-index: 1;
+    background-color: greenyellow;
+    box-shadow: 0 0 20px 20px greenyellow;
   }
 </style>
