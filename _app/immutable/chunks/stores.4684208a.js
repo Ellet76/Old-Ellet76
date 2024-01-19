b8ebaaaf4af460e20a2bc53c1808a1b0798071bd
Ellet76 <122363941+Ellet76@users.noreply.github.com>
2024-01-19 13:34:27 +0000

    Deploying to gh-pages from @ Ellet76/Ellet76.github.io@423454f0272864979c7ad3241b0dc0ee904b19e4 🚀

---
+++ _app/immutable/chunks/stores.4684208a.js
@@ -0,0 +1,1 @@
+import"./index.0dada26d.js";import{d as e}from"./singletons.65d89055.js";const r=()=>{const s=e;return{page:{subscribe:s.page.subscribe},navigating:{subscribe:s.navigating.subscribe},updated:s.updated}},b={subscribe(s){return r().page.subscribe(s)}};export{b as p};
---
+++ _app/immutable/nodes/3.39616a02.js
@@ -0,0 +1,7 @@
+import{S as v,i as E,s as k,C as L,k as u,q as S,a as b,J as q,l as _,m as $,r as C,h as r,c as I,n as h,D as d,b as y,E as N,F as z,G as A,g as D,d as F}from"../chunks/index.0dada26d.js";function G(o){let s,l,f,i,a,c;const m=o[1].default,t=L(m,o,o[0],null);return{c(){s=u("link"),l=u("style"),f=S(`nav {
+      margin-left: 10%;
+      margin-right: 10%;
+    }`),i=b(),a=u("main"),t&&t.c(),this.h()},l(e){const n=q("svelte-mz6nhh",document.head);s=_(n,"LINK",{rel:!0,href:!0}),l=_(n,"STYLE",{});var p=$(l);f=C(p,`nav {
+      margin-left: 10%;
+      margin-right: 10%;
+    }`),p.forEach(r),n.forEach(r),i=I(e),a=_(e,"MAIN",{class:!0});var g=$(a);t&&t.l(g),g.forEach(r),this.h()},h(){h(s,"rel","stylesheet"),h(s,"href","/pico.min.css"),h(a,"class","container")},m(e,n){d(document.head,s),d(document.head,l),d(l,f),y(e,i,n),y(e,a,n),t&&t.m(a,null),c=!0},p(e,[n]){t&&t.p&&(!c||n&1)&&N(t,m,e,e[0],c?A(m,e[0],n,null):z(e[0]),null)},i(e){c||(D(t,e),c=!0)},o(e){F(t,e),c=!1},d(e){r(s),r(l),e&&r(i),e&&r(a),t&&t.d(e)}}}function J(o,s,l){let{$$slots:f={},$$scope:i}=s;return o.$$set=a=>{"$$scope"in a&&l(0,i=a.$$scope)},[i,f]}class M extends v{constructor(s){super(),E(this,s,J,G,k,{})}}export{M as component};
