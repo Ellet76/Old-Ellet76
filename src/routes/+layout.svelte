f91650da95b975b3bb3e5a7ae65feebd768025c5
Linus Scott <linsco@edu.umea.se>
2024-03-27 08:50:53 +0100

    new fixes

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,18 +5,7 @@
 
 <nav>
   <a href="/Old-Ellet76/">Hem</a>
-  <a href="/Old-Ellet76/memory">Memory</a>
-  <a href="/Old-Ellet76/telltale">Telltale</a>
-  <a href="/Old-Ellet76/photoapp">PhotoApp</a>
-  <a href="/Old-Ellet76/login">Log in</a>
-  <a href="/Old-Ellet76/registerButBetter">Register</a>
-  <a href="/Old-Ellet76/Todo">Todo</a>
-  <a href="/Old-Ellet76/chat">Chatbot</a>
-  <a href="/Old-Ellet76/search">Search</a>
-  <a href="/Old-Ellet76/scroll">Scroll</a>
-  <a href="/Old-Ellet76/blog">Blogg</a>
-  <a href="/Old-Ellet76/RunAround-Game">Game</a>
-  <a href="/Old-Ellet76/Webshop">Webshop</a>
+
   <a target="_blank" rel="noreferrer" href="https://github.com/Ellet76/Ellet76.github.io">GitHub</a>
 </nav>
 
