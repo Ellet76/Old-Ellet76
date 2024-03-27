ebbe5b33939a9abbfcc3e0968890ac80bf6e0c41
Linus Scott <linsco@edu.umea.se>
2024-03-27 08:40:46 +0100

    fixes

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,19 +4,19 @@
 
 
 <nav>
-  <a href="/">Hem</a>
-  <a href="/memory">Memory</a>
-  <a href="/telltale">Telltale</a>
-  <a href="/photoapp">PhotoApp</a>
-  <a href="/login">Log in</a>
-  <a href="/registerButBetter">Register</a>
-  <a href="/Todo">Todo</a>
-  <a href="/chat">Chatbot</a>
-  <a href="/search">Search</a>
-  <a href="/scroll">Scroll</a>
-  <a href="/blog">Blogg</a>
-  <a href="/RunAround-Game">Game</a>
-  <a href="/Webshop">Webshop</a>
+  <a href="/Old-Ellet76/">Hem</a>
+  <a href="/Old-Ellet76/memory">Memory</a>
+  <a href="/Old-Ellet76/telltale">Telltale</a>
+  <a href="/Old-Ellet76/photoapp">PhotoApp</a>
+  <a href="/Old-Ellet76/login">Log in</a>
+  <a href="/Old-Ellet76/registerButBetter">Register</a>
+  <a href="/Old-Ellet76/Todo">Todo</a>
+  <a href="/Old-Ellet76/chat">Chatbot</a>
+  <a href="/Old-Ellet76/search">Search</a>
+  <a href="/Old-Ellet76/scroll">Scroll</a>
+  <a href="/Old-Ellet76/blog">Blogg</a>
+  <a href="/Old-Ellet76/RunAround-Game">Game</a>
+  <a href="/Old-Ellet76/Webshop">Webshop</a>
   <a target="_blank" rel="noreferrer" href="https://github.com/Ellet76/Ellet76.github.io">GitHub</a>
 </nav>
 
